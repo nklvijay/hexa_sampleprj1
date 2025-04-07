91ca56d68b65c86bc223b294b618d6a7a53f2242
VijayKumar MS <nklvijay@gmail.com>
2025-04-07 14:34:38 +0530

    airthmetic_op
    arithmetic

---
+++ prg1_basic_arithmetic.py
@@ -0,0 +1,20 @@
+x=10
+y=3
+
+
+add=x+y
+sub=x-y
+div=x/y
+mul=x*y
+
+floor_div=x//y
+mod=x%y
+exp=x**y
+
+print('Addition   :  ',add)
+print('Division   :  ',div)
+print('Floor Division   :  ',floor_div)
+print('Multiplication   :  ',mul)
+print('Modulo    :  ',mod)
+print('Exponent    :  ',exp)
+
